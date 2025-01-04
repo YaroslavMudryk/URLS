ef5fc6e943d61ebfe893670db68ee598dc49a1ec
Yaroslav Mudryk <yaroslav_mudryk@epam.com>
2025-01-04 20:14:19 +0200

    Add correlationId middleware

---
+++ src/URLS.Api/Infrastructure/Constants/HttpHeadersConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace URLS.Api.Infrastructure.Constants;
+
+public class HttpHeadersConstants
+{
+    public const string CorrelationId = "x-correlation-id";
+}
--- src/URLS.Api/Program.cs
+++ src/URLS.Api/Program.cs
@@ -41,6 +41,7 @@ public class Program
         }
 
         app.UseMiddleware<ETagMiddleware>();
+        app.UseMiddleware<CorrelationContextMiddleware>();
         app.UseMiddleware<LoggingMiddleware>();
         app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
@@ -85,8 +86,9 @@ public class Program
 
     private static void RegisterMiddlewares(IServiceCollection services)
     {
-        services.AddScoped<GlobalExceptionHandlerMiddleware>();
-        services.AddScoped<LoggingMiddleware>();
-        services.AddScoped<ETagMiddleware>();
+        services.AddTransient<CorrelationContextMiddleware>();
+        services.AddTransient<GlobalExceptionHandlerMiddleware>();
+        services.AddTransient<LoggingMiddleware>();
+        services.AddTransient<ETagMiddleware>();
     }
 }
